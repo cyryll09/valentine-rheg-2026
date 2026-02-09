9009d300019d3ab1346254bb4553acfedc953d0f
cyryll09 <43166325+cyryll09@users.noreply.github.com>
2026-02-09 08:10:33 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     
-    valentineName: "Rhegine",
+    valentineName: "Rhegine Montemayor Duka",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
